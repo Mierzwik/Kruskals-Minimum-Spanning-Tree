f29c7dcaf43b71cde2878dc0209c602f93feef95
Bryan Mierzwik <B.Mierzwik@gmail.com>
2015-08-08 13:18:46 -0700

    Refactored error handling

--- unionfind.py
+++ unionfind.py
@@ -14,12 +14,14 @@ def find(nodes, U):
 
 def union(nodes,U0,U1):
     U1_temp = find(nodes,U1)
-    if U1_temp == None:
-        print('Union failed ' + str(U1) + ' not found')
-        return None
     U0_temp = find(nodes,U0)
-    if U0_temp == None:
-        print('Union failed ' + str(U0) + ' not found')
+    if U1_temp == None or U0_temp == None:
+        failed = []
+        if U0_temp == None:
+            failed.append(U0)
+        if U1_temp == None:
+            failed.append(U1)
+        print('\nUnion failed: Element(s) ' + str(failed) + ' not found\n')
         return None
     nodes[U0_temp] = U1_temp
     return U1_temp
