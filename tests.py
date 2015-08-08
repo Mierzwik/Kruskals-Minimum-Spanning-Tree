639e3e88a23005d6ee55a1c5a8456133bbb8d1a7
Bryan Mierzwik <B.Mierzwik@gmail.com>
2015-08-08 13:08:28 -0700

    Fixed test and added tests to ensure elements exist before changing

--- tests.py
+++ tests.py
@@ -1,10 +1,40 @@
 import unionfind
+from pprint import pprint
+# Method to pretty print the nodes and what they point to
+def pp():
+    pprint(nodes, width=1)
 
-E = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+E = ['a','b','c','d','e','f','g','h','i','j']
+print('Nodes initialized to all equal null')
 nodes = unionfind.init(E)
-print(nodes.items())
+pp()
 
-unionfind.union(nodes,1,3)
+print('Union a and c, node a points to node c')
+unionfind.union(nodes,'a','c')
+pp()
 
-print(nodes.items())
-print(unionfind.find(nodes,3))
+print('Union b and d, Node b points to node d')
+unionfind.union(nodes,'b','d')
+pp()
+
+print('Union c and f, node c points to node f')
+unionfind.union(nodes,'c','f')
+pp()
+
+print('Union j with b, this points canonical rep of j to rep of b')
+#node j points to node b, but
+# b points to d, therefore j
+# will point to d
+unionfind.union(nodes,'j','b')
+pp()
+
+print('Union h with a, points canonical rep of h to rep of a')
+#h will point to c for reasons above
+unionfind.union(nodes,'h','a')
+pp()
+
+print('Union f with j, point canonical rep of f to rep of j')
+# This should make f point to d
+unionfind.union(nodes,'f','j')
+pp()
+
--- unionfind.py
+++ unionfind.py
@@ -1,5 +1,3 @@
-
-
 def init(E):
     nodes = {}
     for e in E:
@@ -7,12 +5,22 @@ def init(E):
     return nodes
 
 def find(nodes, U):
+    if U not in nodes:
+        print('Find failed: ' + str(U) + ' not found')
+        return None
     if nodes[U] == None:
         return U
     return find(nodes,nodes[U])
 
 def union(nodes,U0,U1):
-    temp = find(nodes,U0)
-    nodes[find(nodes,U1)] = temp
-    return temp
+    U1_temp = find(nodes,U1)
+    if U1_temp == None:
+        print('Union failed ' + str(U1) + ' not found')
+        return None
+    U0_temp = find(nodes,U0)
+    if U0_temp == None:
+        print('Union failed ' + str(U0) + ' not found')
+        return None
+    nodes[U0_temp] = U1_temp
+    return U1_temp
 
