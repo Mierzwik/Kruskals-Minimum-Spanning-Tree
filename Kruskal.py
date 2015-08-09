6458caa79528b9c331ab4cd29a0a4921a6f07ce0
Bryan Mierzwik <B.Mierzwik@gmail.com>
2015-08-08 20:20:35 -0700

    Still working on this, but wanted to save progress

--- Kruskal.py
+++ Kruskal.py
@@ -3,6 +3,7 @@ from re import split
 from pprint import pprint
 from math import floor
 import operator
+
 def setup():
     #Get information from file
     theFile = open('city-pairs.txt')
@@ -12,6 +13,8 @@ def setup():
     #Split data string into list
     rawData = split('\n',rawData)
     rawData = filter(bool,rawData)
+    
+    cities = list()
 
     #Create and return sorted data in list
     stuff = list()
@@ -20,19 +23,34 @@ def setup():
         temp = line.split()
         item.append(temp[0])
         item.append(temp[1])
-        
         item.append(int(temp[2]))
+        cities.append(temp[0])
+        
         stuff.append(item)
-             
-    return sorted(stuff, key=operator.itemgetter(2))
+
+    cities = sorted(set(cities))
+    return sorted(stuff, key=operator.itemgetter(2)),cities
 
 # Method to perform Kruskal's Algorithm    
-def kruskal(data):
-    tree = []
-    pprint(data)
-    return tree
+def kruskal(data,cities):
+    distance = 0
+    #Create disjoined forests
+    cities = init(cities)
+    for edge in range(len(data)):     
+        path = data.pop(0)
+        # Since this is bi-directional, popping the next element
+        # gets rid of the extra one we have.
+        #data.pop(0)
+        if cities[str(path[0])] == None:
+            union(cities, str(path[0]),str(path[1]))
+    return cities,distance
 
-stuff = setup()
-result = kruskal(stuff)
-print(result)
-print(type([]))
+data,cities = setup()
+result,distance = kruskal(data,cities)
+pprint(result)
+print("Distance of minimum spanning tree: " + str(distance))
+
+
+
+
+
